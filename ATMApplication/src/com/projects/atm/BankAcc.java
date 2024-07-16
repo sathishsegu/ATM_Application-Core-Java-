d6a3d9d4cf37b12aac4a62aeac05fa4504fa81b3
Sathish Segu <107339089+sathishsegu@users.noreply.github.com>
2024-07-16 14:28:25 +0530

    Initial Commit
    First Commit to add all the project files

---
+++ ATMApplication/src/com/projects/atm/BankAcc.java
@@ -0,0 +1,22 @@
+package com.projects.atm;
+
+public class BankAcc {
+	private double balance;
+
+	public BankAcc() {
+		super();
+	}
+
+	public BankAcc(double balance) {
+		super();
+		this.balance = balance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
